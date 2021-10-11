235854e215146c872c10a543db9d61a2804bb740
Alexandr Kotov <Mr1Hiroshi@yandex.ru>
2021-10-11 15:33:05 +0500

    Project finish

--- js/index.js
+++ js/index.js
@@ -4,16 +4,16 @@ const popupButtonEdit = document.querySelector('.profile__button-edit');
 
 const popupForm = document.querySelector('.popup__form');
 
-const popupInputTitle = document.querySelector('.popup__input_title');
+const popupInputTypeName = document.querySelector('.popup__input_type_name');
 const profileTitle = document.querySelector('.profile__title');
 
-const popupInputText = document.querySelector('.popup__input_text');
+const popupInpuTypeDescription = document.querySelector('.popup__input_type_description');
 const profileText  = document.querySelector('.profile__subtitle');
 
 function openPopup(){
     popup.classList.add('popup_opened')
-    popupInputTitle.value = profileTitle.textContent
-    popupInputText.value = profileText.textContent
+    popupInputTypeName.value = profileTitle.textContent
+    popupInpuTypeDescription.value = profileText.textContent
 };
 
 function closePopup(){
@@ -26,8 +26,8 @@ popupButtonClose.addEventListener('click', closePopup);
 
 function sumitForm(event){
     event.preventDefault()
-    profileTitle.textContent = popupInputTitle.value
-    profileText.textContent = popupInputText.value
+    profileTitle.textContent = popupInputTypeName.value
+    profileText.textContent = popupInpuTypeDescription.value
     closePopup()
 };
 
