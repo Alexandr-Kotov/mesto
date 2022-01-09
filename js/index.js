d2df7d765af7fa0c09ff3191072d30cf65db5439
Alexandr Kotov <Mr1Hiroshi@yandex.ru>
2022-01-09 19:40:41 +0500

    Project 7.0-50%

--- js/index.js
+++ js/index.js
@@ -1,32 +1,8 @@
-const initialCards = [
-    {
-      name: 'Ёлка',
-      link: 'https://images.unsplash.com/photo-1609009755337-df397a1aa480?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
-    },
-    {
-      name: 'Париж',
-      link: 'https://images.unsplash.com/photo-1639562427517-593c20c356b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },
-    {
-      name: 'Собака',
-      link: 'https://images.unsplash.com/photo-1611253550249-a1fc4e0b90e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80'
-    },
-    {
-      name: 'Печенье',
-      link: 'https://images.unsplash.com/photo-1639610806386-661d00972b55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-    },
-    {
-      name: 'Кофе',
-      link: 'https://images.unsplash.com/photo-1632588417408-2ae5f733e4ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1493&q=80'
-    },
-    {
-      name: 'Автомобиль',
-      link: 'https://images.unsplash.com/photo-1639696307229-0c944a9126e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-    }
-  ]; 
-  
+import {initialCards} from './initialCards.js';
+import Card from './Card.js';
 
-const template = document.querySelector('.template').content;
+export {popupImage, popupFigcaption, popupBig, openPopup};
+
 const listCard = document.querySelector('.elements');
 const popups = document.querySelectorAll('.popup');
 const popupEdit = document.querySelector('.popup_edit');
@@ -44,51 +20,14 @@ const popupImage = document.querySelector('.popup__image');
 const popupFigcaption = document.querySelector('.popup__figcaption');
 const buttonEdit = document.querySelector('.profile__button-edit');
 const buttonAdd = document.querySelector('.profile__button-add');
-const buttonCloseEdit = document.querySelector('.popup__close_edit');
-const buttonCloseAdd = document.querySelector('.popup__close_add');
-const buttonCloseBig = document.querySelector('.popup__close_big');
-const buttonSaveForm = document.querySelector('.popup__save_add')
+const buttonSaveForm = document.querySelector('.popup__save_add');
 
-//LIST CARD
+function renderNewCard(card) {
+  const newCard = new Card(card, '.template');
+  const cardElement = newCard.createCard();
 
-function createCard(card){
-    const element = template.querySelector('.elements__card').cloneNode(true);
-    const cardTitle = element.querySelector('.elements__title');
-    const cardImg = element.querySelector('.elements__img');
-    cardTitle.innerText = card.name;
-    cardImg.src = card.link;
-    cardImg.alt = card.name;
-    element.querySelector('.elements__button').addEventListener('click', likeCardButton);
-    element.querySelector('.elements__delete').addEventListener('click', deleteCardButton);
-
-    //OPEN FULL SCREEN 
-
-    cardImg.addEventListener('click', function () {
-      popupImage.src = cardImg.src;
-      popupImage.alt = cardImg.name;
-      popupFigcaption.textContent = cardTitle.textContent;
-      openPopup(popupBig);
-    })
-    
-    return element;
-};
-
-function renderCard(card){
-    listCard.prepend(card);
-};
-
-//LIKE CARD BUTTON 
-
-function likeCardButton(evt) {
-    evt.target.classList.toggle('elements__button_active');
-};
-
-//DELETE CARD BUTTON
-
-function deleteCardButton(evt){
-    const element = evt.target.closest('.elements__card');
-    element.remove();
-};
+  listCard.prepend(cardElement);
+}
 
 //POPUP OFF/ON
 
@@ -140,15 +79,15 @@ function addNewCard(event){
     name : name,
     link : href
   }
-  renderCard(createCard(card))
+  renderNewCard(card)
   closePopup();
   inputHrefName.value = "";
   inputHref.value = "";
   buttonSaveForm.setAttribute('disabled', true);
 };
 
-initialCards.forEach(function (item){
-  renderCard(createCard(item))
+initialCards.forEach(function (card){
+  renderNewCard(card)
 });
 //LISTENERS
 
